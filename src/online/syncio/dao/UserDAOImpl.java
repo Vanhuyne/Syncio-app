f17f1eab5ac5544ed028afb824faea871402c6e0
van huy <thanvanhuyy@gmail.com>
2023-07-20 10:38:36 +0700

    huy

--- src/online/syncio/dao/UserDAOImpl.java
+++ src/online/syncio/dao/UserDAOImpl.java
@@ -1,23 +1,40 @@
 package online.syncio.dao;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.result.InsertOneResult;
+import java.util.ArrayList;
 import java.util.List;
 import online.syncio.model.User;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 public class UserDAOImpl implements UserDAO {
 
     private MongoDatabase database;
+    List<User> listUser = new ArrayList<>();
 
     public UserDAOImpl(MongoDatabase database) {
         this.database = database;
     }
 
     @Override
-    public boolean add(User t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean add(User user) {
+        MongoDatabase database = MongoDBConnect.getDatabase();
+        MongoCollection<User> collection = database.getCollection("users", User.class);
+        try {
+
+            InsertOneResult result = collection.insertOne(user);
+
+            MongoDBConnect.closeConnection();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
@@ -38,7 +55,45 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public List<User> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // Lấy kết nối và collection từ MongoDBConnect
+        MongoDatabase database = MongoDBConnect.getDatabase();
+        MongoCollection<User> collection = database.getCollection("users", User.class);
+
+        try {
+            FindIterable<User> users = collection.find();
+
+            // listUser
+            for (User user : users) {
+                listUser.add(user);
+            }
+            MongoDBConnect.closeConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listUser;
+    }
+
+    public User authentication(String username, String password) {
+        MongoDatabase database = MongoDBConnect.getDatabase();
+        MongoCollection<User> collection = database.getCollection("users", User.class);
+
+        Bson filter = Filters.eq("username", username);
+        User user = collection.find(filter).first();
+
+        if (user == null) {
+            MongoDBConnect.closeConnection();
+            return null;
+        }
+        
+        // check password
+        if (user.getPassword().equals(password)) {
+            MongoDBConnect.closeConnection();
+            return user;
+        } else {
+            MongoDBConnect.closeConnection();
+            return null;
+        }
     }
 
 }
