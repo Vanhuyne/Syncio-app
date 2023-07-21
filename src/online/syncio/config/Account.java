fb6df1299584d7bbfa7ce6152561f1794104542f
Duong <77065902+56duong@users.noreply.github.com>
2023-07-21 09:16:28 +0700

    update apikey send email

--- src/online/syncio/config/Account.java
+++ src/online/syncio/config/Account.java
@@ -4,4 +4,6 @@ public class Account {
     public static final String CONNECTION_STRING = "mongodb+srv://SyncioApp:HCzZXCzshY5fv1au@cluster0.r5fkiwd.mongodb.net/";
     
     public static final String DATABASE_NAME = "Syncio";
+
+    public static final String API_KEY_SENDEMAIL = "FEE7C772E79AA83098363CEB89509257DE0C13DE3DE0D6E627CAA04E1F0FBB47494CE49E178799D17AB2E0BFA61B1BB9";
 }
