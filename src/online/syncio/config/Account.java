1271bec38e3874604d0d61612939562b6e2c94b5
Duong <77065902+56duong@users.noreply.github.com>
2023-08-28 12:45:46 +0700

    Update Account.java

--- src/online/syncio/config/Account.java
+++ src/online/syncio/config/Account.java
@@ -7,21 +7,21 @@ public class Account {
     /**
      * The MongoDB connection string for connecting to the database.
      */
-    public static final String CONNECTION_STRING = "mongodb+srv://SyncioApp:HCzZXCzshY5fv1au@cluster0.r5fkiwd.mongodb.net/";
+    public static final String CONNECTION_STRING = "mongodb+srv://YourAppUser:YourSecurePassword@your-cluster.mongodb.net/"; // Update this with your actual MongoDB connection string
     
     /**
      * The name of the database used in the application.
      */
-    public static final String DATABASE_NAME = "Syncio";
+    public static final String DATABASE_NAME = "YourAppDatabase"; // Update this with your actual MongoDB database name
 
     /**
      * The API key for sending email notifications.
      */
-    public static final String API_KEY_SENDEMAIL = "FEE7C772E79AA83098363CEB89509257DE0C13DE3DE0D6E627CAA04E1F0FBB47494CE49E178799D17AB2E0BFA61B1BB9";
+    public static final String API_KEY_SENDEMAIL = "YOUR_EMAIL_API_KEY"; // Update this with your actual email API key
    
     /**
      * The URL of the GitHub repository associated with the application.
      */
-    public static final String GITHUB_REPOSITORIE = "https://github.com/56duong/Syncio/";
+    public static final String GITHUB_REPOSITORIE = "https://github.com/yourusername/your-repo/"; // Update this with your app's GitHub repository URL
 
 }
